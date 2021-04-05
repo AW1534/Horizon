e138229e8213af7fcf59920e230bda5fc9b51ab2
AW1534 <addiktedmontage@gmail.com>
2021-04-05 16:44:19 +0100

    Add project files.

---
+++ Browser/AppContainer.cs
@@ -0,0 +1,36 @@
+﻿using EasyTabs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Horizon
+{
+    public partial class AppContainer : TitleBarTabs
+    {
+        public AppContainer()
+        {
+            InitializeComponent();
+            AeroPeekEnabled = false;
+            TabRenderer = new ChromeTabRenderer(this);
+// Icon = Properties.Resources.Icon1;
+        }
+
+        public override TitleBarTab CreateTab()
+        {
+            return new TitleBarTab(this)
+            {
+                Content=new frmMain { Text = "New Tab"}
+            };
+        }
+        private void AppContainer_KeyDown(object sender, KeyEventArgs e)
+        {
+            //frmMain.browser.Load("https://youtube.com");
+        }
+    }
+}
---
+++ Browser/TabStyle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using EasyTabs;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Browser
+{
+    public class TabStyle : BaseTabRenderer
+    {
+        public TabStyle(TitleBarTabs parentWindow) : base(parentWindow)
+        {
+            ForeColor = new System.Drawing.Color();
+        }
+    }
+}
