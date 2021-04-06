b729157767f9aa70c38b6c4b6f67677983f6ff0a
AW1534 <addiktedmontage@gmail.com>
2021-04-06 21:56:14 +0100

    ok

---
+++ Browser/CustomMenuHandler.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using CefSharp;
+using System.Windows.Forms;
+using Horizon;
+
+public class MyCustomMenuHandler : IContextMenuHandler
+{
+
+    string defaultEngine = "DuckDuckGo";
+    public void OnBeforeContextMenu(IWebBrowser browserControl, IBrowser browser, IFrame frame, IContextMenuParams parameters, IMenuModel model)
+    {
+        // Remove any existent option using the Clear method of the model
+        //
+        //model.Clear();
+
+        Console.WriteLine("Context menu opened !");
+
+        // You can add a separator in case that there are more items on the list
+        if (model.Count > 0)
+        {
+            model.AddSeparator();
+        }
+
+
+        // Add a new item to the list using the AddItem method of the model
+        model.AddItem((CefMenuCommand)26500, "Search " + defaultEngine);
+        model.AddSeparator();
+        model.AddItem((CefMenuCommand)26501, "Show DevTools");
+        model.AddItem((CefMenuCommand)26502, "Close DevTools");
+        model.AddSeparator();
+        model.AddItem((CefMenuCommand)26503, "Show Theme Menu");
+        model.AddItem((CefMenuCommand)26503, "print");
+
+
+
+        // Add a separator
+        model.AddSeparator();
+
+        // Add another example item
+        model.AddItem((CefMenuCommand)26503, "Display alert message");
+    }
+
+    public bool OnContextMenuCommand(IWebBrowser browserControl, IBrowser browser, IFrame frame, IContextMenuParams parameters, CefMenuCommand commandId, CefEventFlags eventFlags)
+    {
+        if (commandId == (CefMenuCommand)26500)
+        {
+            MessageBox.Show("(Not Implemented)");
+            return true;
+        }
+        // React to the first ID (show dev tools method)
+        if (commandId == (CefMenuCommand)26501)
+        {
+            browser.GetHost().ShowDevTools();
+            return true;
+        }
+
+        // React to the second ID (show dev tools method)
+        if (commandId == (CefMenuCommand)26502)
+        {
+            browser.GetHost().CloseDevTools();
+            return true;
+        }
+
+        // Any new item should be handled through a new if statement
+
+
+        // Return false should ignore the selected option of the user !
+        return false;
+    }
+
+    public void OnContextMenuDismissed(IWebBrowser browserControl, IBrowser browser, IFrame frame)
+    {
+
+    }
+
+    public bool RunContextMenu(IWebBrowser browserControl, IBrowser browser, IFrame frame, IContextMenuParams parameters, IMenuModel model, IRunContextMenuCallback callback)
+    {
+        return false;
+    }
+}
---
+++ Horizon Installer/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO.Compression;
+namespace Horizon_Installer
+{
+    public partial class Form1 : Form
+    {
+        string filename = "Horizon.zip";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string savelocation = @"C:\Users\addik\source\repos\test\";
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                wc.DownloadProgressChanged += wc_DownloadProgressChanged;
+                wc.DownloadFileAsync(
+                    // Param1 = Link of file
+                    new System.Uri("http://hdownload.6te.net/" + filename),
+                    // Param2 = Path to save
+                    savelocation + filename
+                );
+            }
+        }
+        void wc_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+
+            int progressCompletionAmmount = progressBar1.Maximum / progressBar1.Value;
+
+            if (progressBar1.Maximum == e.ProgressPercentage)
+            {
+                // do whatever you want to do
+            }
+        }
+    }
+}
